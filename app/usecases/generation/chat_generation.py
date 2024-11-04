f62d02b56b8d26c4e0756166bcce2075d0558ddd
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2024-11-04 15:37:14 +0700

    Update with stream option

--- app/usecases/generation/chat_generation.py
+++ app/usecases/generation/chat_generation.py
@@ -24,7 +24,7 @@ def generate_chat(chat_input: str="", summary: str="", language: str="") -> dict
             "prompt": chat_input,
         }
 
-        output = client.run(
+        output = client.stream(
             "meta/meta-llama-3-8b-instruct",
             input=input_data                  
         )
